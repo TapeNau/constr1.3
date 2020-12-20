68ef27d4d5d3c5ddf205e5521c1fbaedcc8430d5
admin <4738551@stud.nau.edu.ua>
2020-12-20 22:10:13 +0200

    Initial Commit

---
+++ src/Producer.java
@@ -0,0 +1,38 @@
+import java.lang.Thread;
+
+class Producer extends Thread {
+
+    // Creating a string buffer 
+    StringBuffer buffer;
+    boolean dp = false;
+
+    // Initializing the string buffer 
+    Producer()
+    {
+        buffer = new StringBuffer(4);
+    }
+
+    // Overriding the run method 
+    public void run()
+    {
+        synchronized (buffer)
+        {
+
+            // Adding the data into the 
+            // buffer 
+            for (int i = 0; i < 4; i++) {
+                try {
+                    buffer.append(i);
+                    System.out.println("Produced " + i);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // Notifying the buffer 
+            System.out.println("Buffer is FUll");
+            buffer.notify();
+        }
+    }
+} 
